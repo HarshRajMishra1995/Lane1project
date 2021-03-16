63ab6adabddace49a2f102d780897555d3122bb6
TJS3 <tjs3@PriyankOldMBP.local>
2021-03-16 14:05:10 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
 const express=require('express');
 const app= express()
-
 const dotenv=require('dotenv');
 dotenv.config();
 const port=process.env.PORT;
 
+const pdf=require("pdf-creator-node");
+const fs=require("fs");
+const path=require("path");
+const html=fs.readFileSync(path.join(__dirname,"./public/template.html"),"utf8");
+
 const bodyparser=require('body-parser')
 app.use(bodyparser.json())
-var urlencodedParser = bodyparser.urlencoded({ extended: false })  
+const urlencodedParser = bodyparser.urlencoded({ extended: false })  
 
 app.use(express.static('public'))
 
+const options={
+    format: "A4",
+    orientation: "portrait",
+    border: "1mm", 
+};
+
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
 
 app.get('/form',(req,res)=>{                                //api to render form
     res.sendFile(path.join(__dirname,'public'))
 })
 
-
 app.post('/response',urlencodedParser,(req,res)=>{          //api to get response
-    const {empname,empid,month,salary}=req.body;
+    let {empname,empid,month,salary}=req.body;
     if(!req.body)
     {
         res.send("missing details")
     }
-    
-    console.log(req.body);
-    console.log(empname);
-    res.status(200).send("your request has been accepted")
+    let users=[
+        {
+            name:empname,
+            empid:empid,
+            month:month,
+            salary:salary
+        }
+    ];
+    let tempFilePath=`./output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+    // var tempFilePath=`/Users/tjs3/Documents/pdf_generator/output/Payslip-${empid}-${Math.floor(new Date().getTime() / 1000)}.pdf`;
+    let document={
+        html:html,
+        data:{
+          users:users
+        },
+        path: tempFilePath,
+        type:""
+    };    
+pdf.create(document,options)
+.then(()=>{
+    let files = fs.createReadStream(tempFilePath);
+    res.writeHead(200, 
+        {'Content-disposition': 'attachment; filename=payslip.pdf'}); //here you can specify file name
+    files.pipe(res); 
+})
+.catch((error)=>{
+console.log(error)
 })
 
-
-
-// app.get("/",(req,res)=>{
-//     res.send("hello");
-// })
-
+});
 
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`);
-})
+});
