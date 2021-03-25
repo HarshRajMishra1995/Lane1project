94a62a7b6faa7a1275e009f9c6e4fcd49c601f3e
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-03-25 16:26:54 +0530

    adjusted running script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT||3000;
 
 const pdf = require("pdf-creator-node");
 const fs = require("fs");
