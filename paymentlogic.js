12e854e73799f06b09e352f838fca7852ff7e38f
harshrajmishra1995 <harshrajmishra.1995@gmail.com>
2021-03-30 14:42:52 +0530

    added paymentLogic file

--- paymentlogic.js
+++ paymentlogic.js
@@ -1,23 +1,26 @@
+function basic(salary)
+{
+    const basicpay=(0.40 * salary).toFixed(2)
+    return basicpay;
+}
+function da(salary)
+{
+    const basicpay=(0.20 * salary).toFixed(2)
+    return basicpay;
+}
+function hra(salary)
+{
+    const basicpay=(0.20 * salary).toFixed(2)
+    return basicpay;
+}function special(salary)
+{
+    const basicpay=(0.20 * salary).toFixed(2)
+    return basicpay;
+}
 
-function basic(salary) {
-    const basicpay = (0.40 * salary / 12).toFixed(2)
-    return basicpay;
-}
-function da(salary) {
-    const basicpay = (0.20 * salary / 12).toFixed(2)
-    return basicpay;
-}
-function hra(salary) {
-    const basicpay = (0.20 * salary / 12).toFixed(2)
-    return basicpay;
-} function special(salary) {
-    const basicpay = (0.20 * salary / 12).toFixed(2)
-    return basicpay;
-}
-
-module.exports = {
+module.exports={
     basic,
     da,
     hra,
     special,
-}
+};
